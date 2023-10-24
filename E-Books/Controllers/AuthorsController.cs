66627968a646fd451aba648b1ab2e0d61848fa46
MohammadBagheriii <132600596+MohammadBagheriii@users.noreply.github.com>
2023-10-24 18:41:39 +0330

    Controllers_for_each_model_added

---
+++ E-Books/Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using E_Books.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Books.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public AuthorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _context.Authors.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ E-Books/Controllers/BookStoresController.cs
@@ -0,0 +1,21 @@
+﻿using E_Books.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Books.Controllers
+{
+    public class BookStoresController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BookStoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var book_stores = await _context.BookStores.ToListAsync();
+            return View();
+        }
+    }
+}
